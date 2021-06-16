b075fe61a873e2ee14e2a6d24ea296d09d146f03
enk <enk>
2021-06-16 17:52:25 -0600

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,3 +4,4 @@ from fabric.context_managers import cd
 def CD():
      with cd('Desktop/eljenkins'): # Me Dirijo a la carpeta donde se encuentra el codigo
         run('docker-compose up -d') # levenato los contenedores
+        return 0
