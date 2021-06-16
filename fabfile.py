19925af545b411a778818713dcef155119093de0
enk <enk>
2021-06-16 17:55:16 -0600

    path de fabric

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,6 @@ from fabric.api import run
 from fabric.context_managers import cd
 
 def CD():
-     with cd('Desktop/eljenkins'): # Me Dirijo a la carpeta donde se encuentra el codigo
+     with cd('/home/norki/Desktop/eljenkins'): # Me Dirijo a la carpeta donde se encuentra el codigo
         run('docker-compose up -d') # levenato los contenedores
         return 0
